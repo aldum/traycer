147e98bf28572cabcd11a850c996a4d5d25f4371
aldum <aldum@artixlinux.org>
2021-01-24 21:55:35 +0100

    [dotty] clean main

--- scala3/src/main/scala/Main.scala
+++ scala3/src/main/scala/Main.scala
@@ -1,14 +1,21 @@
 package pw.aldum.traycer
 
 import Geometry._
-object Main:
-  def msg = "I was compiled by dotty :)"
 
-  def main(args: Array[String]): Unit =
-    println(msg)
-    val p = Point(1,2,3)
-    val v = Vector(1,2,3)
-    println(p)
-    println(v)
-  end main
+object Main extends App:
+  // println(msg)
+  // val p = Point(1,2,3)
+  // val v = Vector(1,2,3)
+  // println(p)
+  // println(v)
+  val c1 = Color(1.5, 0, 0)
+  val c2 = Color(0, .5, 0)
+  val c3 = Color(-0.5, 0, 1)
+  val can = new Canvas(5, 3)
+          .writePixel(0, 0, c1)
+          .writePixel(2, 1, c2)
+          .writePixel(4, 2, c3)
+  println(can.PPM.toPPM)
+  // println(new Canvas(100, 100).PPM.toPPM)
+
 
